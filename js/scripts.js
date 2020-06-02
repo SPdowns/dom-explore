5b708917b3e6bb5676dd9fbc392c91ff07e33199
Sean Downs <spdowns@protonmail.com>
2020-06-02 16:29:40 -0700

    show we know how to manipulate that DOM
    Co-authored-by: James Henager <jameschenager@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   $("button#hello").click(function() {
     $("ul.user").prepend("<li>Hello!</li>");
     $("ul.webpage").prepend("<li>Why hello there!</li>");
-    $("ul#user").children("li").first().click(function() {
+    $("ul.user").children("li").first().click(function() {
       alert('hi');
     });
-    $("ul#webpage").children("li").first().click(function() {
+    $("ul.webpage").children("li").first().click(function() {
       alert('hi');
     });
   });
